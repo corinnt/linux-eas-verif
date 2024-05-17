0ef37537e084dcd5b1125e7b049e3656bcf0449d
corinnt <94402869+corinnt@users.noreply.github.com>
2024-05-17 17:12:14 +0200

    small test proves after putting info wrt sched_domain_span(sd)->bits instead of cpumask_test_cpu

--- v2/prove/C_test_small.c
+++ v2/prove/C_test_small.c
@@ -4,18 +4,20 @@
 #include "../preds.c"
 #include "masks.c"
 
+//this all proves
+
 /*@
 requires 0 <= prev_cpu < small_cpumask_bits; 
-requires \valid(sd); 
+requires \valid_read(sd); 
 
 assigns \result;
 
 behavior in_mask:
-    assumes cpumask_test_cpu(prev_cpu, sched_domain_span(sd));  										
+    assumes sched_domain_span(sd)->bits[prev_cpu]; 
     ensures is_true: \result; 
 
 behavior notin_mask: 
-    assumes !cpumask_test_cpu(prev_cpu, sched_domain_span(sd));  										
+    assumes !sched_domain_span(sd)->bits[prev_cpu]; 									
 	ensures is_false: !\result;
 
 complete behaviors;
@@ -24,10 +26,12 @@ disjoint behaviors;
 bool testing_if_statement(struct sched_domain* sd, int prev_cpu){
     //@ assert sd_not_null: sd != NULL;
     if (cpumask_test_cpu(prev_cpu, sched_domain_span(sd))) {
-        //@ assert sd_in_mask: cpumask_test_cpu(prev_cpu, sched_domain_span(sd)); 
+        // assert sd_in_mask: cpumask_test_cpu(prev_cpu, sched_domain_span(sd)); 
+        //@ assert sd_in_mask: sched_domain_span(sd)->bits[prev_cpu]; 
         return true; 
     } else if (!cpumask_test_cpu(prev_cpu, sched_domain_span(sd))) { // just to make explicit the three cases
-        //@ assert sd_notin_mask: !cpumask_test_cpu(prev_cpu, sched_domain_span(sd)); 
+        // assert sd_notin_mask: !cpumask_test_cpu(prev_cpu, sched_domain_span(sd)); 
+        //@ assert sd_notin_mask: !sched_domain_span(sd)->bits[prev_cpu]; 
         return false; 
     }
 }
