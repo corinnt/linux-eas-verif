5ed59c1aeeb7aecf2a9fb3c92cc0fedd13441536
corinnt <94402869+corinnt@users.noreply.github.com>
2024-06-04 19:17:10 -0700

    clean up

--- v2/preds.c
+++ v2/preds.c
@@ -4,10 +4,6 @@
 #include <stdbool.h>
 #include <limits.h>
 
-//this is just a fn that returns the sd->span of type as a cpumask
-// don't think I need to be able to write to it, and don't think there are any particular limits
-// TODO maybe need the span(array of unsigned longs) to be less than some max value?
-
 /*@
    axiomatic spans_locks_and_archs {
     logic struct cpumask* sched_domain_span(struct sched_domain* sd);
@@ -52,8 +48,12 @@ unsigned long compute_energy(struct energy_env * eenv,
 
 unsigned long capacity_of(int cpu);
 
+/*
+*/
 unsigned long arch_scale_thermal_pressure(int cpu);
 
+/*
+*/
 unsigned long arch_scale_cpu_capacity(int cpu);
 
 void __compiletime_assert_1068();
